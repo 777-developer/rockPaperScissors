eaaa7ffc9297730711edeb4bb91f3067af204406
777-developer <212229513+777-developer@users.noreply.github.com>
2025-10-05 18:49:23 +0300

    modified html, css, js files

--- game.js
+++ game.js
@@ -1,93 +1,139 @@
-let humanScore = 0;
-let computerScore = 0;
+// let humanScore = 0;
+// let computerScore = 0;
+
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText= document.querySelector("#resultText");
+const choiceBths = document.querySelectorAll(".choiceBth");
+let player;
+let computer;
+let result;
+
+choiceBths.forEach(button => button.addEventListener('click', () => {
+
+    player = button.textContent;
+    getComputerChoice();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner();
+}))
+
+
+
+
+
+
 
 
 
 function getComputerChoice() {
-    let computerChoce = Math.random();
-    
-    if (computerChoce < 0.36) {
-        return 'Rock';
-    }
-    else if (computerChoce < 0.65) {
-        return 'Paper';
-    }
-    else {
-        return 'Scissors';
+    let computerChoice = Math.floor(Math.random()*3) + 1;
+
+    switch (computerChoice) {
+        case 1: 
+        computer = 'ROCK';
+        break;
+        case 2:
+        computer = 'PAPER';
+        break;
+        case 3:
+        computer = 'SCISSORS';
+        break; 
     }
 }
 
-function getHumanChoice() {
+
+function checkWinner() {
+    if (player === computer) {
+        return 'Draw!';
+    }
+
+    else if (computer === 'ROCK') {
+        return (player === 'PAPER') ? 'You win!' : 'You lose!';
+    }
+    else if (computer === 'PAPER') {
+        return (player === 'SCISSSORS') ? 'You win!' : 'You lose!';
+    }
+    else if (computer === 'SCISSORS') {
+        return (player === 'ROCK') ? 'You win!' : 'You lose!';
+    }
+
+}
+
+// function getHumanChoice() {
     
-    let result = null;
+//     let result = null;
     
-    let humanChoice = prompt('Please your choice: ', '');
+//     let humanChoice = prompt('Please your choice: ', '');
 
-    if (humanChoice) {
+//     if (humanChoice) {
 
-        humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1).toLowerCase();
+//         humanChoice = humanChoice[0].toUpperCase() + humanChoice.slice(1).toLowerCase();
 
-        if (humanChoice === 'Rock' || humanChoice === 'Scissors' || humanChoice === 'Paper') {
+//         if (humanChoice === 'Rock' || humanChoice === 'Scissors' || humanChoice === 'Paper') {
   
-            result = humanChoice;
+//             result = humanChoice;
             
-        }
-    }
+//         }
+//     }
 
-        return result;
-}
+//         return result;
+// }
 
 
-    function playRound(humanChoice, computerChoice) {
+    // function playRound(humanChoice, computerChoice) {
 
-       if (humanChoice === 'Rock' && computerChoice === 'Paper' || humanChoice === 'Paper' && computerChoice === 'Scissors' || humanChoice === 'Scissors' && computerChoice === 'Rock') {
+    //    if (humanChoice === 'Rock' && computerChoice === 'Paper' || humanChoice === 'Paper' && computerChoice === 'Scissors' || humanChoice === 'Scissors' && computerChoice === 'Rock') {
         
-            if (humanChoice > computerChoice) {
+    //         if (humanChoice > computerChoice) {
 
 
-            humanScore++;  
-            return (`You won! Your choce ${humanChoice} Computer has choice ${computerChoice}`);
+    //         humanScore++;  
+    //         return (`You won! Your choce ${humanChoice} Computer has choice ${computerChoice}`);
             
-         }
+    //      }
 
 
-            else if (computerChoice > humanChoice) {
+    //         else if (computerChoice > humanChoice) {
 
 
-            computerScore++; 
-            return (`Computer won! Computer choice ${computerChoice}. You have choiced ${humanChoice}`);
-        }
+    //         computerScore++; 
+    //         return (`Computer won! Computer choice ${computerChoice}. You have choiced ${humanChoice}`);
+    //     }
 
-    }
+    // }
     
 
-            else  {
+    //         else  {
                
-                humanScore === computerScore;
+    //             humanScore === computerScore;
 
-                return (`Draw! You and computer choiced ${humanChoice, computerChoice}`); 
-        }
+    //             return (`Draw! You and computer choiced ${humanChoice, computerChoice}`); 
+    //     }
     
-    }
+    // }
  
 
 
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
+    // const humanChoice = getHumanChoice();
+    // const computerChoice = getComputerChoice();
 
     
         
     
 
     
-    function playGame() {
+    // function playGame() {
 
 
-        console.log(playRound(humanChoice, computerChoice));
-        console.log(`human ${humanScore}, computer ${computerScore}`);
-    }
+    //     console.log(playRound(humanChoice, computerChoice));
+    //     console.log(`human ${humanScore}, computer ${computerScore}`);
+    // }
 
-    playGame();
+    // playGame();
+
+    
+
     
 
 
